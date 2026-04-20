4bc94b583edfa752e7b7926ebf435f4eee7ef5f8
Elodie Bellier <elodie.bellier@ucdenver.edu>
2026-04-19 19:43:08 -0600

    lets put that back actually

--- src/components/register.js
+++ src/components/register.js
@@ -32,7 +32,7 @@ function Register() {
                 </Form.Group>
 
                 <Form.Group controlId="username">
-                    <Form.Label>Username</Form.Label>
+                    <Form.Label>Email</Form.Label>
                     <Form.Control onChange={updateDetails} value={details.username} autoComplete="username" type="text" placeholder="Enter username" />
                 </Form.Group>
 
